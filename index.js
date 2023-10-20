04992f4dacec6ef4458c77589b2a743e5e1924ef
fatmaist <fatmaistyn@gmail.com>
2023-10-20 16:40:06 +0700

    feat(db) : Create the connection to database the name is movies

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
+var pool = require('./queries')
 
-app.get('/', function(req, res){
-    res.send("Hello this homework!")
+pool.connect((err, res) => {
+    console.log(err)
+    console.log('connected')
 })
 
 app.listen(3000)
